f742a5e0b3d742d25c8eb32ab48f19e53273a3e5
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-05-23 19:25:58 +0530

    Password & Email Validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { validation } from "../utils/formValidation";
 
 const Login = () => {
   const [isSignInForm, setIsSignInFomr] = useState(true);
+  const email = useRef(null);
+  const password = useRef(null);
+  const [errorMessage,setErrorMessage] = useState(null);
 
   const handleFormToggle = () => {
     setIsSignInFomr(!isSignInForm);
+  };
+
+  const handleValidation = () => {
+    const msg = validation(email.current.value,password.current.value);
+    setErrorMessage(msg);
   };
 
   return (
@@ -18,31 +27,41 @@ const Login = () => {
           alt="bg-img"
         ></img>
       </div>
-      <form className=" bg-black bg-opacity-60 w-3/12 h-3/6 absolute z-10 my-36 p-4 mx-auto right-0 left-0 flex justify-center flex-wrap">
+      <form onSubmit={(e)=>(e.preventDefault())} className=" bg-black bg-opacity-60 w-3/12 h-3/6 absolute z-10 my-36 p-4 mx-auto right-0 left-0 flex justify-center flex-wrap">
+          
         <h1 className="p-2 my-4 text-3xl text-white font-bold">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
           <input
             type="text"
-            placeholder="Email Address"
+            placeholder="Name"
             className="p-2 my-2 w-3/4 bg-inherit border placeholder-slate-500 border-solid border-slate-500 rounded-lg text-white"
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-2 my-2 w-3/4 bg-inherit border placeholder-slate-500 border-solid border-slate-500 rounded-lg text-white"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-2 my-2 w-3/4 bg-inherit placeholder-slate-500 border border-solid border-slate-500 rounded-lg text-white"
         />
-        <button className=" bg-red-600 text-white p-2 my-4 w-3/4 rounded-lg">
+        <p className="p-2 mx-10 font-extrabold text-red-500">{errorMessage?errorMessage:"Valid Email & Password"}</p>
+        <button
+          className=" bg-red-600 text-white p-2 my-4 w-3/4 rounded-lg"
+          onClick={handleValidation}
+        >
           Submit
         </button>
-        <p className="p-2 my-2 cursor-pointer text-white" onClick={handleFormToggle}>
+        <p
+          className="p-2 my-2 cursor-pointer text-white"
+          onClick={handleFormToggle}
+        >
           {isSignInForm
             ? "First time here? Sign Up"
             : "Already a user? Sign In"}
---
+++ src/utils/formValidation.js
@@ -0,0 +1,9 @@
+export const validation = (email,password) => {
+    const isEmailValid = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+    if(!isEmailValid) return "Invalid Email Address";
+    if(!isPasswordValid) return "Invalid Password";
+
+    if(isEmailValid && isPasswordValid) return null;
+}
