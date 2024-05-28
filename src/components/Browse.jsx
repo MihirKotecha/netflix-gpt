e449bcd7de3e81edac9cb0f9c776e52520ff93d9
MihirKotecha <mihir.k1@ahduni.edu.in>
2024-05-28 15:52:21 +0530

    Navigate to Login if user not signed in

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,11 +1,21 @@
+import { getAuth } from "firebase/auth";
 import Header from "./Header";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Browse = () => {
-    return(
-        <div>
-            <Header />
-        </div>
-    );
-}
+  const navigate = useNavigate();
+  useEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (!user) navigate("/");
+  }, []);
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 
 export default Browse;
